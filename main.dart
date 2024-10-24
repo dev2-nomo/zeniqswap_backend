8f747014437d407afa20fdd33a4f6613dabe571a
dev2-al <dev2@nomo.app>
2024-10-24 13:55:30 +0200

    fix pipeline

--- main.dart
+++ main.dart
@@ -5,7 +5,7 @@ import 'package:dart_frog/dart_frog.dart';
 
 import 'src/pool_provider.dart';
 
-const version = "1.0.2";
+const version = "1.0.3";
 
 Future<void> init(InternetAddress ip, int port) async {
   unawaited(PairProvider.instance.init());
